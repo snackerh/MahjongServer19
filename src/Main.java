b906268b9532722c04610c3f4603d33e0a14ba47
snackerh <whanlee4@naver.com>
2019-01-01 05:10:01 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void runServer() {
+		boolean running = true;
+		
+		try {
+			while(running) {
+				System.out.println("Server: waiting for new connection");
+				
+			}
+		} catch (Exception e) {
+			
+		}
+	}
+	
+}
